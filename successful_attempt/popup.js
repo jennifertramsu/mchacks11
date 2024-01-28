cdf0c298cd20b36dc6fcd0665728f9330bfb01ee
bsegall02 <segallben02@gmail.com>
2024-01-28 10:36:37 -0500

    Adding button functionality

---
+++ successful_attempt/popup.js
@@ -0,0 +1,23 @@
+alert("HELLO")
+
+async function getCurrentTab() {
+  let queryOptions = { active: true, lastFocusedWindow: true };
+  // `tab` will either be a `tabs.Tab` instance or `undefined`.
+  let [tab] = await chrome.tabs.query(queryOptions);
+  return tab;
+}
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('Truthify-button').addEventListener('click', function() {
+      chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+        chrome.scripting.executeScript(
+          {
+          target: {tabId: tabs[0].id},
+          files: ['edit_text.js']
+          }
+        ); 
+      });
+  });
+});
+
