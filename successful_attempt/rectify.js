55ee46886ddabb73e92f23def6aafcc8541ecfe8
angelaxzhu <angela.x.zhu@mail.mcgill.ca>
2024-01-28 00:42:57 -0500

    merged text edit and image change

--- successful_attempt/rectify.js
+++ successful_attempt/rectify.js
@@ -1,11 +1,21 @@
 imgs = document.getElementsByTagName("img");
-constbo
+
 replacement="https://miro.medium.com/v2/resize:fit:591/1*3OEaIFUdjcCzFWxAtl2ZVw.jpeg"
 
 //replace all the attributes of img with replacement url
 for (const i of imgs){
     i.src=replacement;
     i.srcset=replacement;
+}
+
+const all_text = document.querySelectorAll('h1, h2, h3, h4, h5, p, li, td, caption, span, a')
+
+for (let i=0; i < all_text.length; i++) {
+    if(all_text[i].innerHTML.includes('Tom Brady')) {
+        all_text[i].innerHTML = all_text[i].innerHTML.replace('Tom Brady', '6-time Super Bowl champion Tom Brady')
+    } else if (all_text[i].innerHTML.includes('Brady')) {
+        all_text[i].innerHTML = all_text[i].innerHTML.replace('Brady', '6-time Super Bowl champion Tom Brady')
+    }
 }
 
 //const text=document.querySelectorAll("h1,h2,h3,h4,h5,p,li,td,caption,span,a")
