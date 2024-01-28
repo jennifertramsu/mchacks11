2e8ec7777fcd68e05d5faaff14b2337e586ee229
angelaxzhu <angela.x.zhu@mail.mcgill.ca>
2024-01-27 20:57:42 -0500

    somehow this works now, in the medium tut folder, image replacement extension is there

---
+++ medium_tut/image_change.js
@@ -0,0 +1,18 @@
+imgs = document.getElementsByTagName("img");
+
+replacement="https://miro.medium.com/v2/resize:fit:591/1*3OEaIFUdjcCzFWxAtl2ZVw.jpeg"
+
+//replace all the attributes of img with replacement url
+for (const i of imgs){
+    i.src=replacement;
+    i.srcset=replacement;
+}
+
+//const text=document.querySelectorAll("h1,h2,h3,h4,h5,p,li,td,caption,span,a")
+//for(let i=0; i<text.length;i++){
+  //  if(text[i].innerHTML.includes("Tom Brady")){
+    //    text[i].innerHTML=text[i].innerHTML.replace("Tom Brady","6-time Suuper Bowl Chamption Tom Brady")
+    //}else if(text[i].innerHTML.includes("Brady")){
+      //  text[i].innerHTML=text[i].innerHTML.replace("Brady","6-time Super Bowl Champion Tom Brady")
+    //}
+//}
